347d13284fd46f87e1d2c3d42fa848194425797f
joshuapink2002 <83262978+joshuapink2002@users.noreply.github.com>
2025-09-22 13:38:55 +0900

    SaveButton

---
+++ SooTopDowngame/Assets/SaveController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UnityEngine.Audio;
+using System.IO;
+using Unity.Cinemachine;
+
+public class SaveController : MonoBehaviour
+{
+    private string saveLocation;
+    void Start()
+    {
+        saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
+
+        LoadGame();
+    }
+
+    public void SaveGame()
+    {
+        SaveData saveData = new SaveData
+        {
+            playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position,
+            mapBoundary = FindAnyObjectByType<CinemachineConfiner2D>().BoundingShape2D.gameObject.name
+        };
+        File.WriteAllText(saveLocation,JsonUtility.ToJson(saveData));
+    }
+
+    public void LoadGame()
+    {
+        if (File.Exists(saveLocation))
+        {
+            SaveData saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
+
+            GameObject.FindGameObjectWithTag("Player").transform.position = saveData.playerPosition;
+
+            FindAnyObjectByType<CinemachineConfiner2D>().BoundingShape2D = GameObject.Find(saveData.mapBoundary).GetComponent<PolygonCollider2D>();
+        }
+        else
+        {
+            SaveGame();
+        }
+    }
+}
---
+++ SooTopDowngame/Assets/SaveData.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class SaveData
+{
+    public Vector3 playerPosition;
+    public string mapBoundary; //The boundary name for the map
+
+}
