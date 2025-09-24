5e66b64312c1e882af928c1aa9bfcc1da1cffb61
joshuapink2002 <83262978+joshuapink2002@users.noreply.github.com>
2025-09-24 20:16:21 +0900

    Player pick item

---
+++ SooTopDowngame/Assets/Scripts/PlayerItemCollector.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UnityEngine.Audio;
+
+public class PlayerItemCollector : MonoBehaviour
+{
+    private InventoryController inventoryController;
+
+    private void Start()
+    {
+        inventoryController = FindAnyObjectByType<InventoryController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Item"))
+        {
+            Item item = collision.GetComponent<Item>();
+            if (item != null)
+            {
+                //Add item inventory
+                bool itemAdded = inventoryController.AddItem(collision.gameObject);
+                if (itemAdded)
+                {
+                    Destroy(collision.gameObject);
+                }
+            }
+        }
+    }
+}
